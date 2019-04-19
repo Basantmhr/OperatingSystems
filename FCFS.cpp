b47aeaffafb14a7d06e1f31ce9c37a62fcc5fb91
sbm virdi <shubhamvirdic3@gmail.com>
2019-04-19 23:28:17 +0530

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -3,16 +3,17 @@ using namespace std;
 int main() 
 { 
 int n,tdistance=0; 
-int DiskQueue[5000]; 
-int cur_pos; 
+int DiskQueue[]={86, 1470, 913, 1774, 948, 1509, 1022, 1750, 130}; 
+int cur_pos=143; 
 cout<<"\n FCFS Disc-Scheduler\n"<<endl;
-cout<<"Enter no. of requests pending for processing::";
-cin>>n;
-cout<<"\nEnter the cylinder no. for corresponding requests(IN FCFS ORDER)::"<<endl;
-for(int i=0;i<n;++i) {
-cin>>DiskQueue[i];
-}
-cout<<"Enter current position of disc arm::";
-cin>>cur_pos;
+for(int j=0;j<9;++j) 
+{ 
+if((DiskQueue[j]-cur_pos)>0)
+tdistance=tdistance +DiskQueue[j] - cur_pos; 
+else 
+tdistance= tdistance - DiskQueue[j] + cur_pos; 
+cur_pos=DiskQueue[j]; 
+} 
+cout<<"Total distance moved by the arm is::"<<tdistance<<endl;
 
 }
